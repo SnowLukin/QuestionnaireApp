bb21e9cbf5c889530872389749c0eab9e1d61760
Snow Lukin <everyoursden@gmail.com>
2021-12-20 21:22:00 +0300

    Initial Commit

---
+++ QuestionnaireApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionnaireApp
+//
+//  Created by Snow Lukin on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
